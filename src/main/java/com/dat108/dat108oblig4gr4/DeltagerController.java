601f9e3a00c293032d521d08d994f391fd03b64a
JohannesNA <186506@stud.hvl.no>
2025-10-15 13:13:41 +0200

    Start

--- src/main/java/com/dat108/dat108oblig4gr4/DeltagerController.java
+++ src/main/java/com/dat108/dat108oblig4gr4/DeltagerController.java
@@ -1,4 +1,16 @@
 package com.dat108.dat108oblig4gr4;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class DeltagerController {
+
+    @GetMapping
+    public String alleDeltagere(Model model) {
+
+        return "deltagerView";
+    }
+
 }
